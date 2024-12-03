af2da5caea6dc8279399aa92167d5f7707d07721
nqthinhh <thinhskt2k2@gmail.com>
2024-12-03 12:45:58 +0700

    Create lesson03-many-to-many-new-composite-key

--- src/main/java/entity/GroupAccount.java
+++ src/main/java/entity/GroupAccount.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,20 +13,32 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "group_account")
 public class GroupAccount {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @EmbeddedId
+    private PrimaryKey pk;
 
     @ManyToOne
     @JoinColumn(name = "group_id", referencedColumnName = "id", nullable = false)
+    @MapsId("group_id")
     private Group group;
 
     @ManyToOne
     @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = false)
+    @MapsId("account_id")
     private Account account;
 
     @Column(name = "joined_at", nullable = false, updatable = false)
     @CreationTimestamp
     private LocalDateTime joinedAt;
+
+
+    @Embeddable
+    @Getter
+    @Setter
+    public static class PrimaryKey implements Serializable {
+        @Column(name = "group_id", nullable = false)
+        private int groupId;
+
+        @Column(name = "account_id", nullable = false)
+        private int accountId;
+    }
 }
--- src/main/java/frontend/Program.java
+++ src/main/java/frontend/Program.java
@@ -15,45 +15,40 @@ public class Program {
     public static void main(String[] args) {
        try (var factory = HibernateUtil.buildSessionFactory()){
             factory.inTransaction(session -> {
-               var group1 = new Group();
-               group1.setName("Hibernate");
-               session.persist(group1);
+                        var group1 = new Group();
+                        group1.setName("Hibernate");
+                        session.persist(group1);
 
-                var group2 = new Group();
-                group2.setName("Spring Framework");
-                session.persist(group2);
+                        var group2 = new Group();
+                        group2.setName("Spring Framework");
+                        session.persist(group2);
 
-                   var account = new Account();
-                   account.setName("Thinh");
-                   account.setEmail("thinh@gmai.com");
-                   session.persist(account);
+                        var account = new Account();
+                        account.setName("Thinh");
+                        account.setEmail("thinh@gmai.com");
+                        session.persist(account);
 
-                    var account2 = new Account();
-                    account2.setName("Thuy");
-                    account2.setEmail("thuy@gmai.com");
-                    session.persist(account2);
+                        var account2 = new Account();
+                        account2.setName("Thuy");
+                        account2.setEmail("thuy@gmai.com");
+                        session.persist(account2);
 
-                    var groupAccount1 = new GroupAccount();
-                    groupAccount1.setAccount(account);
-                    groupAccount1.setGroup(group1);
-                    session.persist(groupAccount1);
+                        var groupAccount1 = new GroupAccount();
+                        var pk1 = new GroupAccount.PrimaryKey();
+                        pk1.setGroupId(1);
+                        pk1.setAccountId(1);
+                        groupAccount1.setPk(pk1);
+                        session.persist(groupAccount1);
 
-                    var groupAccount2 = new GroupAccount();
-                    groupAccount2.setAccount(account2);
-                    groupAccount2.setGroup(group1);
-                    session.persist(groupAccount2);
+                        var groupAccount2 = new GroupAccount();
+                        var pk2 = new GroupAccount.PrimaryKey();
+                        pk2.setGroupId(1);
+                        pk2.setAccountId(2);
+                        groupAccount2.setPk(pk2);
+                        session.persist(groupAccount2);
 
-                    var groupAccount3 = new GroupAccount();
-                    groupAccount3.setAccount(account);
-                    groupAccount3.setGroup(group2);
-                    session.persist(groupAccount3);
 
-                var groupAccount4 = new GroupAccount();
-                groupAccount4.setAccount(account);
-                groupAccount4.setGroup(group2);
-                session.persist(groupAccount4);
-            });
-
+                    });
             factory.inSession(session -> {
                         var hql = "FROM Group";
                         var groups = session
